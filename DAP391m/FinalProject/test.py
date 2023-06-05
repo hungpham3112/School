e0774d9c401e788eec87432e2a8abe6b76eb7518
HungPham <phamhung20022015@gmail.com>
2023-06-05 21:42:15 +0700

    new commit

---
+++ DAP391m/FinalProject/test.py
@@ -0,0 +1,46 @@
+import face_recognition
+import cv2
+import numpy as np
+
+# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
+# other example, but it includes some basic performance tweaks to make things run a lot faster:
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
+
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+
+# Get a reference to webcam #0 (the default one)
+
+def main():
+    print("[ droidcam.py ] - Initializing...")
+
+    # Opening video stream of ip camera via its url
+    cap = cv2.VideoCapture("http://192.168.137.100:3112/video")
+
+    # Corrective actions printed in the even of failed connection.
+    if cap.isOpened() is not True:
+        print ('Not opened.')
+        print ('Please ensure the following:')
+        print ('1. DroidCam is not running in your browser.')
+        print ('2. The IP address given is correct.')
+
+    # Connection successful. Proceeding to display video stream.
+    while cap.isOpened() is True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Turning your frames into grayscale
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow('frame',frame)
+        # cv2.imshow('gray',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__=='__main__':
+    main()
