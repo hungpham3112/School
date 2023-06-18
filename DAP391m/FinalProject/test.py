7252eafcfe8a2123f920fbca5d7a526a58900f86
Hung Pham <phamhung20022015@gmail.com>
2023-06-19 01:22:15 +0700

    Update mutiple files

--- DAP391m/FinalProject/test.py
+++ DAP391m/FinalProject/test.py
@@ -1,23 +1,18 @@
-import face_recognition
+from keras.models import load_model
+from time import sleep
+from tensorflow.keras.utils import img_to_array
+from keras.preprocessing import image
 import cv2
 import numpy as np
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
-
 def main():
     print("[ droidcam.py ] - Initializing...")
+    face_classifier = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+    classifier =load_model('./fer_model.h5')
 
+    emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
     # Opening video stream of ip camera via its url
-    cap = cv2.VideoCapture("http://192.168.137.100:3112/video")
+    cap = cv2.VideoCapture("http://192.168.137.101:3112/")
 
     # Corrective actions printed in the even of failed connection.
     if cap.isOpened() is not True:
@@ -27,20 +22,41 @@ def main():
         print ('2. The IP address given is correct.')
 
     # Connection successful. Proceeding to display video stream.
-    while cap.isOpened() is True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
+    while True:
+        _, frame = cap.read()
+        labels = []
+        if not frame.empty():
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Perform further processing on the frame
+        else:
+            # Handle the case when the frame is empty
+            print("Empty frame received from the video source.")
+        faces = face_classifier.detectMultiScale(gray)
 
-        # Turning your frames into grayscale
-        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+            roi_gray = gray[y:y+h,x:x+w]
+            roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
 
-        cv2.imshow('frame',frame)
-        # cv2.imshow('gray',gray)
+            if np.sum([roi_gray])!=0:
+                roi = roi_gray.astype('float')/255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi,axis=0)
+
+                prediction = classifier.predict(roi)[0]
+                label=emotion_labels[prediction.argmax()]
+                label_position = (x,y)
+                cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+            else:
+                cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+        cv2.imshow('Emotion Detector',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
+
+
 
 if __name__=='__main__':
     main()
